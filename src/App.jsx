2cab8afd6776477eea877cfc69b9d20b592c3baa
Yeonjae Lee <lyeonj@sookmyung.ac.kr>
2025-07-22 20:34:46 +0900

    fix: Header 다시하기 버튼 함수 수정 및 불필요한 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
 import IntroPage from './pages/IntroPage';
-import TestPage from './pages/TestPage';
 import TestResultPage from './pages/TestResultPage';
 import FemaleTestPage from './pages/FemaleTestPage';
 import MaleTestPage from './pages/MaleTestPage';
 import StatsPage from './pages/StatsPage';
 
-// page for test
-import PageForTest from './pages/PageForTest';
-
 function App() {
+  const location = useLocation();
+
   return (
     <Routes>
       <Route path="*" element={<Navigate to="/" replace />} />
       <Route path="/" element={<IntroPage />} />
-      <Route path="/test" element={<TestPage />} />
-      <Route path="/test-female" element={<FemaleTestPage />} />
-      <Route path="/test-male" element={<MaleTestPage />} />
+      <Route 
+        path="/test-female" 
+        element={<FemaleTestPage 
+        key={location.key}/>} 
+      />
+      <Route 
+        path="/test-male" 
+        element={<MaleTestPage 
+        key={location.key}/>} 
+      />
       <Route path="/test-result" element={<TestResultPage />} />
       <Route path='/stats' element={<StatsPage />} />
-
-      {/* page for test */}
-      <Route path="/page-for-test" element={<PageForTest />} />
     </Routes>
   );
 }
